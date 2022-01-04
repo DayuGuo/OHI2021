8845d2cc9210934163ec0659f4fe7e8c939c12a7
Dayo <dayuguo1129@outlook.com>
2022-01-04 14:40:31 +0800

    new

---
+++ CDI 分布情况.R
@@ -0,0 +1,24 @@
+library(readr)
+data <- read_csv("CDI 分布情况.csv")
+library(ggridges)
+library(ggplot2)
+
+data <- data %>%
+  gather(key="text", value="value") %>%
+  mutate(text = gsub("\\.", " ",text)) %>%
+  mutate(value = round(as.numeric(value),0))
+
+# Plot
+data %>%
+  mutate(text = fct_reorder(text, value)) %>%
+  ggplot( aes(y=text, x=value,  fill=text)) +
+  geom_density_ridges(quantile_lines = TRUE, quantiles = 2) +
+  theme_ridges() +
+  theme(
+    legend.position="none",
+    panel.spacing = unit(0.1, "lines"),
+    strip.text.x = element_text(size = 8)
+  ) +
+  xlab("Score") +
+  ylab("Index")
+
